834ba39f4de1facc12074768c8b37e9bb5b3babb
Himani-R-Patel <37513640+Himani-R-Patel@users.noreply.github.com>
2019-07-28 14:56:34 -0400

    UI design pattern file

---
+++ PowerMemento.java
@@ -0,0 +1,14 @@
+
+public class PowerMemento {
+  
+  private Double state;
+
+  public PowerMemento(Double state) {
+    this.state = state;
+  }
+
+  public Double getState() {
+    return state;
+  }
+
+}
